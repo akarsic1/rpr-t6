033c72d30f423a2680d231fb599b4d9360f3a813
Amel <akarsic1@etf.unsa.ba>
2018-11-29 17:38:49 +0100

    Dodao sam sve dijelove formulara.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -22,10 +24,10 @@ public class Main extends Application {
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
+        grid.setHgap(5);
+        grid.setVgap(5);
 
-        Text welcome = new Text("Popunite sljdeći formular(sva polja su obavezna):");
+        Text welcome = new Text("Popunite sljedeći formular: ");
         welcome.setFont(Font.font("Tahoma", FontWeight.LIGHT, 19));
         grid.add(welcome,0,0);
 
@@ -53,14 +55,89 @@ public class Main extends Application {
         txtjmbg.setPromptText("JMBG");
         grid.add(txtjmbg,1,7);
 
-        Label datum = new Label("Datum rođenja: ");
-        grid.add(datum, 0 ,8);
-        TextField txtdatum = new TextField();
-        txtdatum.setPromptText("datum rođenja");
-        grid.add(txtdatum,1,8);
+        Label datumRodjenja = new Label("Datum rodjenja: ");
+        grid.add(datumRodjenja,0,8);
+        DatePicker dat = new DatePicker();
+        dat.setPromptText("datum rođenja");
+        grid.add(dat,1,8);
+
+        Label mjestRodjenja = new Label("Mjesto rodjenja: ");
+        ObservableList<String> mjestoRodjenja = FXCollections.observableArrayList(
+                "Sarajevo","Tuzla","Zenica","Mostar","Bihać","Goražde"
+        );
+        grid.add(mjestRodjenja,0,9);
+        final ComboBox mjesto = new ComboBox(mjestoRodjenja);
+        mjesto.setEditable(true);
+        mjesto.setPromptText("mjesto rođenja");
+        grid.add(mjesto,1,9);
+
+        Label kontakAdresa = new Label ("Kontakt adresa:");
+        TextField adresa = new TextField();
+        adresa.setPromptText("adresa");
+        grid.add(kontakAdresa,0,13);
+        grid.add(adresa ,1,13);
 
 
-        primaryStage.setScene(new Scene(grid, 1000, 500));
+        Label kontakTelefon = new Label ("Kontakt telefon:");
+        TextField telefon = new TextField();
+        telefon.setPromptText("telefon");
+        grid.add(kontakTelefon,0,14);
+        grid.add(telefon ,1,14);
+
+
+        Label email = new Label ("Email:");
+        TextField emaila = new TextField();
+        emaila.setPromptText("email");
+        grid.add(email,0,15);
+        grid.add(emaila ,1,15);
+
+        Label smjer = new Label("Smjer: ");
+        ObservableList<String> smje = FXCollections.observableArrayList(
+                "Automatika i elektronika","Računarstvo i informatika","Telekomunikacije","Razvoj softvera","Elektroenergetika"
+        );
+        grid.add(smjer,0,19);
+        final ComboBox smjero = new ComboBox(smje);
+        smjero.setPromptText("smjer");
+        grid.add(smjero,1,19);
+
+        Label godinaStudija = new Label("Godina studija: ");
+        grid.add(godinaStudija,0,20);
+        ObservableList<String> god = FXCollections.observableArrayList(
+                "Prva","Druga","Treća"
+        );
+        final ComboBox godina = new ComboBox(god);
+        grid.add(godina,1,20);
+
+        Label ciklusStudija = new Label ("Ciklus studija: ");
+        grid.add(ciklusStudija,0,21);
+        ObservableList<String> cik = FXCollections.observableArrayList("Bachlor", "Master","Doktorski studij", "Stručni studij");
+        final ComboBox cikl = new ComboBox(cik);
+        grid.add(cikl,1,21);
+
+        CheckBox ch1 = new CheckBox();
+        ch1.setText("Redovan");
+        grid.add(ch1,1,22);
+        CheckBox ch2 = new CheckBox();
+        ch2.setText("Redovan samofinansirajući");
+        grid.add(ch2,1,23);
+
+        Label borackaKategorija = new Label("Da li pripadate nekoj od boračkih kategorija? : ");
+        grid.add(borackaKategorija,0,24);
+        CheckBox cb1 = new CheckBox();
+        cb1.setText("DA");
+        grid.add(cb1, 1,24);
+        TextField kojaBO = new TextField();
+        kojaBO.setPromptText("Ako DA, kojoj?");
+        grid.add(kojaBO,2,24);
+        CheckBox cb2 = new CheckBox();
+        cb2.setText("NE");
+        grid.add(cb2, 1,25);
+
+        Button btn = new Button();
+        btn.setText("Potvrdi");
+        grid.add(btn,15,26);
+
+        primaryStage.setScene(new Scene(grid, 1000, 600));
         primaryStage.show();
     }
 
